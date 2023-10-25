0f247dab1c4b544ffb4d13b91c9c5c8c36e0e20a
Your Name <youremail@yourdomain.com>
2023-10-25 08:49:10 +0600

    first commit

---
+++ bin/october_6.dart
@@ -0,0 +1,5 @@
+import 'package:october_6/october_6.dart' as october_6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${october_6.calculate()}!');
+}
---
+++ test/october_6_test.dart
@@ -0,0 +1,8 @@
+import 'package:october_6/october_6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

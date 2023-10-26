fc1eb51135a15cc02fed98b697b60690baebcd4b
Your Name <youremail@yourdomain.com>
2023-10-27 01:09:04 +0600

    commit

--- bin/october_6.dart
+++ bin/october_6.dart
@@ -1,5 +1,34 @@
+import 'dart:ffi';
+
 import 'package:october_6/october_6.dart' as october_6;
 
-void main(List<String> arguments) {
-  print('Hello world: ${october_6.calculate()}!');
+/*void main() {
+ Circle obj = Circle(color: 'Red', r: 50); 
+ print(obj.getRadius());
+ print(obj.getColor());
+
+}
+
+
+class Circle{
+  double r;
+  String color;
+  Circle({required this.color, required this.r});
+  double getRadius(){
+    return r;
+  }
+
+  String getColor(){
+    return color;
+  }
+}
+*/
+
+
+void main(){
+
+}
+
+Class Student{
+  String
 }

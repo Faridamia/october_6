dbcf50732a81b8b2893e61ee1ece27e28bca07d0
Your Name <youremail@yourdomain.com>
2023-11-01 01:54:49 +0600

    commit

--- bin/october_6.dart
+++ bin/october_6.dart
@@ -24,11 +24,88 @@ class Circle{
 }
 */
 
-
+/*
 void main(){
+Student Ali = Student(name: 'Ali', age: 21, score: [50, 40, 20],
+);
+
+Ali.printAll();
+}
+
+class Student{
+  String name;
+  int age;
+  List<int> score;
+  Student({required this.name, required this.age, required this. score});
+
+  double srScore() {
+    int summ = 0;
+    for(int i = 0; i < score.length; i++) {
+      summ += score[i];
+    }
+    return summ / score.length;
+  }
+
+
+void printAll() {
+  print('$age $name $score ${srScore()}');
+}
+
+}*/
+
+/*void main() {
+Rectangle  a = Rectangle (height: 100, width: 200);
+print(a.getP());
+print(a.getS());
+}
+
+class Rectangle {
+  double width;
+  double height;
+  Rectangle({required this.width, required this.height});
+  double getP(){
+   return (width + height) * 2;
+  }
+double getS(){
+  return width * height;
+}
+}*/
+
+/*void main(){
+BankAccount a = BankAccount(name: 'MyBankAcc', nomerScheta: 273190);
+a.deposite(summ: 200);
+a.showBalance();
+a.withdrawAll(summ: 500);
+a.showBalance();
+}
+
+class BankAccount{
+  String name;
+  double balance = 1000;
+  int nomerScheta;
+
+  BankAccount({required this.name, required this.nomerScheta});
+
+  void deposite({required double summ}) {
+    balance += summ;
+  }
+    
+void withdrawAll({required double summ}) {
+  balance -= summ;
 
 }
 
-Class Student{
-  String
+void showBalance(){
+  print(balance);
+}
+}*/
+
+void main() {}
+
+class Car {
+  String name;
+  double model;
+  int year;
+
+  Car({required this.name, required this.model, required this.year});
 }
